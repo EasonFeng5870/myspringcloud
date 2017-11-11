18ac46ad61beadc225e934c01687828074bfa03e
fengyingsheng <fengyingsheng@dangdang.com>
2017-11-11 15:45:44 +0800

    my  spring cloud project.

--- config-server/src/main/java/com/eason/core/config/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/eason/core/config/configserver/ConfigServerApplication.java
@@ -1,7 +1,12 @@
 package com.eason.core.config.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * config server.
@@ -9,6 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author feng yingsheng
  */
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
---
+++ eureka-server/src/main/java/com/eason/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.eason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心
+ *
+ * @author feng yingsheng
+ */
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
